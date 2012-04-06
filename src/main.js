477a50568c6946f5727c76c38fd01711d2745e84
user2 <user2@ubuntu.Belkin>
2012-04-06 06:02:19 -0700

    update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
+	res.end('NODEJS\n');
     }).listen(8000);
 console.log('Server running at http://0.0.0.0:8000/');
