574f9e9f57390698e4b6809eba77f58f0e3b68c7
unknown <Pappana@timbuktu40.(none)>
2012-04-22 15:11:20 -0700

    index

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,7 @@
  * GET home page.
  */
 
-function strencode( data ) {
-  return unescape( encodeURIComponent( JSON.stringify( data ) ) );
-}
-
-function strdecode( data ) {
-  return JSON.parse( decodeURIComponent( escape ( data ) ) );
-}
 
 exports.index = function(req, res, nodes, cb){
-  console.log('encoding');
-  var eNodes = strencode(nodes);
-  console.log(eNodes);
   res.render('index', { title: 'Amarblog2', nodes : nodes }, cb);
 };
