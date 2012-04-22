63c43fe4da30ce29d4921478b61943f0f3d9fa1e
unknown <Pappana@timbuktu40.(none)>
2012-04-21 21:10:56 -0700

    layout

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,17 @@
  * GET home page.
  */
 
+function strencode( data ) {
+  return unescape( encodeURIComponent( JSON.stringify( data ) ) );
+}
+
+function strdecode( data ) {
+  return JSON.parse( decodeURIComponent( escape ( data ) ) );
+}
+
 exports.index = function(req, res, nodes, cb){
-  res.render('index', { title: 'Amarblog', nodes : nodes }, cb);
+  console.log('encoding');
+  var eNodes = strencode(nodes);
+  console.log(eNodes);
+  res.render('index', { title: 'Amarblog2', nodes : JSON.parse(eNodes) }, cb);
 };
